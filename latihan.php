719f7c5baaabfbd18df34ea4a6072aa352208f2b
rahadian <rahadian630@gmail.com>
2022-05-30 08:59:56 +0700

    menambah file baru

---
+++ latihan.php
@@ -0,0 +1,24 @@
+<!--php : server side (mengelola)-->
+<!--cara menulis syntax PHP-->
+<?php
+//menampilkan data ke layar
+// echo "Hello World";
+var_dump($nama);
+$nama = "Rahadian";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
